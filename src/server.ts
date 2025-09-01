8f19c9628ca986d983574b85485b8dcb651385bc
Edu-Marinho <marinhojcc@hotmail.com>
2025-09-01 13:52:18 -0300

    convertendo Typescript para Javascript

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { app } from "@/app";
+import { env } from "./env";
 
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is runnimg on port ${PORT}`))
+app.listen(env.PORT, () => {
+    console.log(`Server is running on port ${env.PORT}`);
+});
